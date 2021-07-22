ccc203139a4d6ec2fb39107030cc0c619ddf6163
mzdws <8580712+mzdws@user.noreply.gitee.com>
2021-07-22 09:12:03 -0400

    test

--- src/identify/consts.js
+++ src/identify/consts.js
@@ -5,5 +5,5 @@ const libp2pVersion = require('../../package.json').version
 
 module.exports.PROTOCOL_VERSION = '0.1.0'
 module.exports.AGENT_VERSION = `js-libp2p/${libp2pVersion}`
-module.exports.MULTICODEC_IDENTIFY = '/id/1.0.0'
-module.exports.MULTICODEC_IDENTIFY_PUSH = '/id/push/1.0.0'
+module.exports.MULTICODEC_IDENTIFY = 'id/1.0.0'
+module.exports.MULTICODEC_IDENTIFY_PUSH = 'id/push/1.0.0'
